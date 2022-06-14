ef2afdecb3e557e047b646b5ea62ec0699972ed8
Tori Acer <vrice360@gmail.com>
2022-06-14 11:13:16 -0500

    add offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const webpack = require("webpack");
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
+// const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
 
-module.exports = {
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: __dirname + "/dist"
+    path: path.join(__dirname + "/dist"),
+    filename: "[name].bundle.js"
   },
   module: {
     rules: [
       {
-        test: /\.jpg$/,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
           {
             loader: 'file-loader',
@@ -25,7 +27,7 @@ module.exports = {
               name(file) {
                 return '[path][name].[ext]';
               },
-              publicPath: function(url) {
+              publicPath(url) {
                 return url.replace('../', '/assets/');
               }
             }
@@ -39,12 +41,33 @@ module.exports = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+      analyzerMode: 'static'
     })
-  ],      
+    // insert here maybe
+  ],
   mode: 'development'
 };
+
+module.exports = config;
+
+
+
+// * for "insert here maybe"
+ // new WebpackPwaManifest({
+    //   name: "Food Event",
+    //   short_name: "Foodies",
+    //   description: "An app that allows you to view upcoming food events.",
+    //   background_color: "#01579b",
+    //   theme_color: "#ffffff",
+    //   fingerprints: false,
+    //   inject: false,
+    //   icons: [{
+    //     src: path.resolve("assets/img/icons/icon-512x512.png"),
+    //     sizes: [96, 128, 192, 256, 384, 512],
+    //     destination: path.join("assets", "icons")
+    //   }]
+    // })
