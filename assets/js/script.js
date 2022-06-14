aa5fb90c7cddf1bfb5610233f4212e24ee8a753e
Tori Acer <vrice360@gmail.com>
2022-06-14 13:44:22 -0500

    aloow users to download the application

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
-require("bootstrap");
+require('bootstrap');
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
 
 $(document).ready(function() {
-    // First image is hard coded in index.html
-    const carouselSlides = [
-      {
-        title: "We travel all over the US",
-        subtitle: "Check out our schedule!",
-        img: img1,
-        btnText: "View Schedule",
-        btnUrl: "schedule.html"
-      },
-      {
-        title: "Our food is seriously the bomb!",
-        subtitle: "What are you waiting for?",
-        img: img2,
-        btnText: "Purchase Tickets",
-        btnUrl: "tickets.html"
-      }
-    ];
-
+  // First image is hard coded in index.html
+  const carouselSlides = [
+    {
+      title: "We travel all over the US",
+      subtitle: "Check out our schedule!",
+      img: img1,
+      btnText: "View Schedule",
+      btnUrl: "schedule.html"
+    },
+    {
+      title: "Our food is seriously the bomb!",
+      subtitle: "What are you waiting for?",
+      img: img2,
+      btnText: "Purchase Tickets",
+      btnUrl: "tickets.html"
+    },
+  ];
+  
   carouselSlides.forEach((slide, i) => {
     $('.carousel-inner').append(`
   <div class="carousel-item fullscreen-carousel" style="background-image: url('${slide.img}')">
